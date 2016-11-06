5f351dd87a6ba4356a5ce04acae92f477ec1acae
Mohammad Kamrul Islam <kamrulislam.bd95@gmail.com>
2016-11-07 02:50:58 +0600

    Initial commit

---
+++ app/src/main/java/com/example/mdkamrul/player/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.mdkamrul.player;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import static android.R.attr.name;
+import static com.example.mdkamrul.player.R.layout.customlayout;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    ListView listViewSong;
+    private ArrayList<Song> songArrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listViewSong = (ListView) findViewById(R.id.listViewSong);
+        songArrayList = new ArrayList<Song>();
+
+        getSong();
+
+        //Toast.makeText(getApplicationContext(),songArrayList.get(2).getTitle().toString(),Toast.LENGTH_LONG).show();
+        //Toast.makeText(getApplicationContext(),"Smile",Toast.LENGTH_LONG).show();
+        ArrayAdapter<Song> songArrayAdapter = new ArrayAdapter<Song>(getApplicationContext(),android.R.layout.simple_list_item_1,songArrayList);
+        listViewSong.setAdapter(songArrayAdapter);
+
+    }
+
+    public  String getSongTitle (){
+        Song song = new Song();
+        return  song.getTitle();
+    }
+
+    public void getSong (){
+        ContentResolver musicResolver = getContentResolver();
+        Uri Musicuri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor musicCursor = musicResolver.query(Musicuri,null,null,null,null);
+        if (musicCursor!=null && musicCursor.moveToNext()){
+            int idColumn = musicCursor.getColumnIndex(android.provider.MediaStore.Audio.Media._ID);
+            int titleColumn = musicCursor.getColumnIndex(android.provider.MediaStore.Audio.Media.TITLE);
+            int artistColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+            int duration =  musicCursor.getColumnIndex(MediaStore.Audio.Media.DURATION);
+            do {
+                long thisid = musicCursor.getLong(idColumn);
+                String titleName= musicCursor.getString(titleColumn);
+                String artistName = musicCursor.getString(artistColumn);
+                Double songDuration = musicCursor.getDouble(duration);
+                songArrayList.add(new Song(thisid,titleName,artistName,songDuration));
+            }
+            while (musicCursor.moveToNext());
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/mdkamrul/player/Song.java
@@ -0,0 +1,51 @@
+package com.example.mdkamrul.player;
+
+/**
+ * Created by mdkamrul on 07-Nov-16.
+ */
+public class Song {
+
+    public long id;
+    private  String title;
+    private  String artist;
+    private  Double duration;
+
+    public Double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Double duration) {
+        this.duration = duration;
+    }
+
+
+
+    public Song (long id,String title,String artist,Double duration){
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+    }
+    public Song (){}
+
+    public  void setTitle (String title){this.title = title;}
+    public  long getId (){
+        return id;
+    }
+    public  String getTitle (){
+        return  title;
+    }
+    public  String getArtist (){
+        return  artist;
+    }
+
+    @Override
+    public String toString() {
+        return
+
+                title + '\n' +
+                artist + '\n'+
+                duration
+                ;
+    }
+}
